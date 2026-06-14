b95579cfa3174d1610877ca176be7a0a5f2100f8
arnavkulkarni148-ux <arnavkulkarni148@gmail.com>
2026-06-14 15:10:53 +0530

    Add files via upload

---
+++ Assignment-2/Q1.py
@@ -0,0 +1,20 @@
+value = 18
+value1 = 3.45
+value2 = 3+5j
+value3 = 18
+
+# Value
+print(value)
+print(value1)
+print(value2)
+
+# Type
+print(type(value))
+print(type(value1))
+print(type(value2))
+
+# Memory Address
+print(id(value))
+print(id(value1))
+print(id(value2))
+print(id(value3))
---
+++ Assignment-2/Q6.py
@@ -0,0 +1,14 @@
+print("Enter the first Number: ")
+no1 = int(input())
+print("Enter the second Number: ")
+no2 = int(input())
+
+add = no1 + no2
+sub = no1 - no2
+mup = no1 * no2
+div = no1 / no2
+
+print("The addition is : ",add)
+print("The subtraction is : ",sub)
+print("The multiplication is : ",mup)
+print("The division is : ",div)
